fbc140c67f985468d41129857f78f91a661879e0
bassemhelal18 <77381611+bassemhelal18@users.noreply.github.com>
2024-07-29 14:16:01 +0300

    Update shahidu.py

--- sites/shahidu.py
+++ sites/shahidu.py
@@ -24,7 +24,7 @@ if cConfig().getSetting('global_search_' + SITE_IDENTIFIER) == 'false':
     logger.info('-> [SitePlugin]: globalSearch for %s is deactivated.' % SITE_NAME)
 
 # Domain Abfrage
-DOMAIN = cConfig().getSetting('plugin_'+ SITE_IDENTIFIER +'.domain', 'shhid4u.cam')
+DOMAIN = cConfig().getSetting('plugin_'+ SITE_IDENTIFIER +'.domain', 'shaheed4u.net')
 URL_MAIN = 'https://' + DOMAIN + '/'
 
 
