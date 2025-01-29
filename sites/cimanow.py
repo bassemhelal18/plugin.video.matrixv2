866e3591e0b90340de06a399a89d9eb60b735b88
bassemhelal18 <77381611+bassemhelal18@users.noreply.github.com>
2025-01-29 18:54:42 +0200

    Revert "Update cimanow.py"
    This reverts commit ae77e8108d794dc0348bc962713cf43ed5f092f0.

--- sites/cimanow.py
+++ sites/cimanow.py
@@ -229,6 +229,7 @@ def showHosters():
             elif 'cimanow' in sUrl:
                 sName = 'CimaNow'
                 sUrl = sUrl +'|AUTH=TLS&verifypeer=false&Referer='+URL_MAIN
+                sUrl = quote(sUrl, '/:=&?|')
             hoster = {'link': sUrl, 'name': sName, 'displayedName':sName+' '+sQuality, 'quality': sQuality} # Qualität Anzeige aus Release Eintrag
             if 'cimanow' in sUrl:
                 hoster.update({ 'resolved': True})
