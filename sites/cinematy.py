a5c3fde7d888959b93cf8ec3f2d57975f35733ff
bassemhelal18 <77381611+bassemhelal18@users.noreply.github.com>
2025-02-14 21:31:32 +0200

    Update cinematy.py

--- sites/cinematy.py
+++ sites/cinematy.py
@@ -26,7 +26,7 @@ if cConfig().getSetting('global_search_' + SITE_IDENTIFIER) == 'false':
     logger.info('-> [SitePlugin]: globalSearch for %s is deactivated.' % SITE_NAME)
 
 # Domain Abfrage
-DOMAIN = cConfig().getSetting('plugin_'+ SITE_IDENTIFIER +'.domain', 'cdn-01.cinematy.click')
+DOMAIN = cConfig().getSetting('plugin_'+ SITE_IDENTIFIER +'.domain', 'cmaty.shop')
 URL_MAIN = 'https://' + DOMAIN + '/'
 
 
