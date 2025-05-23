0156d9fdcb79d462581f1e636efdea87810cc2f6
bassemhelal18 <77381611+bassemhelal18@users.noreply.github.com>
2025-05-23 20:57:24 +0300

    Update arabseed.py

--- sites/arabseed.py
+++ sites/arabseed.py
@@ -26,7 +26,7 @@ if cConfig().getSetting('global_search_' + SITE_IDENTIFIER) == 'false':
     logger.info('-> [SitePlugin]: globalSearch for %s is deactivated.' % SITE_NAME)
 
 # Domain Abfrage
-DOMAIN = cConfig().getSetting('plugin_'+ SITE_IDENTIFIER +'.domain', 'asd.quest')
+DOMAIN = cConfig().getSetting('plugin_'+ SITE_IDENTIFIER +'.domain', 'asd.rest')
 URL_MAIN = 'https://' + DOMAIN + '/'
 
 
